e93ea107aa6226edcd8321258a11756fef845e0c
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2024-06-02 16:49:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,22 +8,22 @@ from dotenv import load_dotenv
 
 load_dotenv("config.env")
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7363047700:AAHde9Y3KdEVQCA5uFMO_pvBdF2konFvf1I")
+APP_ID = int(os.environ.get("APP_ID", "26730559"))
+API_HASH = os.environ.get("API_HASH", "54e0fd326f54b4ea91fdcbdf98e3cf4e")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002177610937"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5908553001"))
 PORT = os.environ.get("PORT", "8080")
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "postgres://emzqisqp:cbKWoSPa1aAa77CFBxvOI9prHR_irlBP@rain.db.elephantsql.com/emzqisqp")
 DB_NAME = os.environ.get("DATABASE_NAME", "geezfsub")
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001970835627"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001660822900"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001266185679"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hallo {first}\n\nsaya bisa menyimpan file dan membagikan dengan mudah.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    for x in (os.environ.get("ADMINS", "5908553001").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
